818678f5d196d6d36ec29a891c1de4f72f177814
Kynan Stewart Hughes <mail@kynan.net.au>
2020-08-29 21:02:51 +1000

    Gererated

--- public/js/App.js
+++ public/js/App.js
@@ -1,11 +1,11 @@
 // @flow
-import {h} from "../web_modules/preact.js";
+import { h } from "../web_modules/preact.js";
 import htm from "../web_modules/htm.js";
 import Router from "../web_modules/preact-router.js";
-import {AppProvider} from "./AppContext.js";
+import { AppProvider } from "./AppContext.js";
 import Lines1 from "./Lines1.js";
 import Lines2 from "./Lines2.js";
-import Lines3 from "./Lines3.js";
+import Moon1 from "./Moon1.js";
 const html = htm.bind(h);
 
 /*::
@@ -13,13 +13,13 @@ type Props = {
 };
 */
 const App /*: function */ = (props /*: Props */) => {
-	return html`
+  return html`
     	<${AppProvider} >
     		<${Router} >
-				<${Lines1} path="/" />
+				<${Moon1} path="/" />
 				<${Lines1} path="/lines1" />
 				<${Lines2} path="/lines2" />
-				<${Lines3} path="/lines3" />
+				<${Moon1} path="/moon1" />
     		</${Router}>
     	</${AppProvider} >
   `;
---
+++ public/js/Moon1.js
@@ -0,0 +1,59 @@
+// @flow
+import { h, render } from "../web_modules/preact.js";
+import { useState, useEffect } from "../web_modules/preact/hooks.js";
+import { createStyles, rawStyles } from "../web_modules/simplestyle-js.js";
+import htm from "../web_modules/htm.js";
+
+const RANGE = 10;
+const SCALE = 1;
+const TARGET = "lines3";
+
+const html = htm.bind(h);
+rawStyles({
+  canvas: {
+    margin: "0px",
+    overflow: "hidden",
+  },
+});
+
+/*::
+type Props = {
+  count: typeof Number
+};
+*/
+const Moon = (props /*: Props */) => {
+  useEffect(() => {});
+
+  return html`
+    <a-scene
+      vr-mode-ui="enabled: false"
+      arjs="sourceType: webcam; debugUIEnabled: false;"
+      debug="true"
+    >
+      <a-entity
+        position="0 2 0"
+        look-at="[gps-camera]"
+        scale=".175 .175 .175"
+        gltf-model="url(/img/coal/DBS_ex_01.gltf)"
+        gps-entity-place="latitude: -35.3082237; longitude: 149.1222036;"
+      ></a-entity>
+      <a-camera
+        rotation-reader
+        gps-camera="
+			alert:true;
+			positionMinAccuracy:1000;
+			minDistance: 0.1;
+			maxDistance: 10000;
+			simulateAltitude:0;
+			simulateLatitude:-35.3092237;
+			simulateLongitude:149.1232036;
+		"
+      >
+      </a-camera>
+    </a-scene>
+  `;
+};
+
+// simulateLatitude:-35.3170519;
+// simulateLongitude:149.107418;
+export default Moon;
