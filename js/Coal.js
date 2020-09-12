32cdcb2b71e38570eb2d7d8d5e76349e29fec6f3
Kynan Stewart Hughes <mail@kynan.net.au>
2020-09-12 16:41:21 +1000

    Working but: [1] won't scale past 4000 x and [2] Won't load from the a-assets

--- js/Coal.js
+++ js/Coal.js
@@ -2,6 +2,7 @@
 import { h, render } from "../web_modules/preact.js";
 import { useState, useEffect } from "../web_modules/preact/hooks.js";
 import { createStyles, rawStyles } from "../web_modules/simplestyle-js.js";
+import screenfull from "../web_modules/screenfull.js";
 import htm from "../web_modules/htm.js";
 
 const html = htm.bind(h);
@@ -17,7 +18,28 @@ type Props = {
 };
 */
 const Coal = (props /*: Props */) => {
-  useEffect(() => {});
+  useEffect(() => {
+    // Events
+    const mainContainer = document.getElementById("goodthing") || null;
+    if (mainContainer !== null) {
+      // Modernizr doesn't have an es module npm package so it's
+      // imported with a <script> tag in `index.html`
+      // $FlowFixMe
+      if (Modernizr.hasEvent("touchend")) {
+        mainContainer.addEventListener(
+          "touchend",
+          () => {
+            // Doesn't work on iPhone ~ https://caniuse.com/#feat=fullscreen
+            // Plus we only want fullscreen on touch devices
+            screenfull.request().then(() /*: void */ => {
+              setTimeout(() /*: void */ => {}, 500);
+            });
+          },
+          { once: true },
+        );
+      }
+    }
+  });
 
   return html`
     <a-scene
@@ -25,12 +47,18 @@ const Coal = (props /*: Props */) => {
       arjs="sourceType: webcam; debugUIEnabled: false;videoTexture: true;"
     >
       <a-entity
-        position="0 200 0"
+        position="0 100 0"
+        scale="4000 4000 4000"
         look-at="[gps-camera]"
-        scale="10 10 10"
-        gltf-model="url(/img/coal/DBS_ex_01.gltf)"
         gps-entity-place="latitude: -35.3082237; longitude: 149.1222036;"
-      ></a-entity>
+      >
+        <a-entity
+          position="0 0 0"
+          scale="0 0 0"
+          gltf-model="url(/img/coal/DBS_ex_01.gltf)"
+        >
+        </a-entity>
+      </a-entity>
       <a-camera
         near="1"
         far="70000"
